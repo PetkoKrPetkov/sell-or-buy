95c5001a1c70c2f22709d5c90f308b1e5642e8ee
PetkoKrPetkov <136084693+PetkoKrPetkov@users.noreply.github.com>
2024-03-09 10:28:40 +0100

    add ng material card template

--- src/app/feature/feature.module.ts
+++ src/app/feature/feature.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page/home-page.component';
 import { SharedModule } from '../shared/shared.module';
-import { CoreModule } from '../core/core.module';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
     HomePageComponent
   ],
   imports: [
-    CommonModule, SharedModule
+    CommonModule, SharedModule, MatButtonModule,
+    MatCardModule
   ],
   exports: [HomePageComponent]
 })
