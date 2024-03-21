cc5c64cdf31888965aed984d31e17b66ed5e48c7
PetkoKrPetkov <136084693+PetkoKrPetkov@users.noreply.github.com>
2024-03-21 11:50:33 +0100

    fixed to dynamic details-page

--- src/app/feature/item-details/item-details.component.ts
+++ src/app/feature/item-details/item-details.component.ts
@@ -1,24 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { ApiService } from 'src/app/api.service';
 import { IItem } from 'src/app/interfaces/item';
 
 @Component({
   selector: 'app-item-details',
   templateUrl: './item-details.component.html',
-  styleUrls: ['./item-details.component.css']
+  styleUrls: ['./item-details.component.css'],
 })
-export class ItemDetailsComponent implements OnInit{
+export class ItemDetailsComponent implements OnInit {
   item: IItem | null = null;
 
-  constructor(private apiService: ApiService) {}
+  constructor(
+    private apiService: ApiService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.apiService.loadItem("65e99cfe4a43bf49278f84c2").subscribe({
-    next: (value) => {
-      this.item = value;
-      console.log(value);
-    },
-    error: (err) => console.error(err),
+    this.activatedRoute.params.subscribe((data) => {
+      console.log(data['_id']);
+      const id = data['_id'];
+
+      this.apiService.loadItem(id).subscribe({
+        next: (value) => {
+          this.item = value;
+          console.log(value);
+        },
+        error: (err) => console.error(err),
+      });
     });
   }
 }
