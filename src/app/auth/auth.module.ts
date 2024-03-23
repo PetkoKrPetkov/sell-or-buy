d9929e9bbd2dd778790ef97beb6cb937058cf650
PetkoKrPetkov <136084693+PetkoKrPetkov@users.noreply.github.com>
2024-03-23 11:14:54 +0100

    add register form validation

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,11 +4,17 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
 import { AuthRoutingModule } from './auth-routing.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent, ProfileComponent],
-  imports: [CommonModule, AuthRoutingModule, FormsModule, SharedModule],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    FormsModule,
+    SharedModule,
+    ReactiveFormsModule,
+  ],
 })
 export class AuthModule {}
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  constructor(private formBuilder: FormBuilder) {}
 
+  form = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    passGroup: this.formBuilder.group(
+      {
+        password: ['', [Validators.required]],
+        confirm_password: ['', [Validators.required]],
+      },
+      { validators: this.matchPasses('password', 'confirm_password') }
+    ),
+  });
+
+  matchPasses(password: string, confirm_password: string): ValidatorFn {
+    return (control) => {
+      const pass = control.get(password)?.value;
+      const rePass = control.get(confirm_password)?.value;
+
+      return pass === rePass ? null : { mismatch: true };
+    };
+  }
+
+  register(): void {
+    if (this.form.invalid) {
+      return;
+    }
+  }
 }
