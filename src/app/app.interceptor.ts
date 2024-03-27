d4b8c3dca7cc7ddbf7fa56487f1227357d736446
PetkoKrPetkov <136084693+PetkoKrPetkov@users.noreply.github.com>
2024-03-27 13:30:42 +0100

    interceptor remake

--- src/app/app.interceptor.ts
+++ src/app/app.interceptor.ts
@@ -1,45 +1,7 @@
-// import {
-//   HTTP_INTERCEPTORS,
-//   HttpEvent,
-//   HttpHandler,
-//   HttpInterceptor,
-//   HttpRequest,
-// } from '@angular/common/http';
-// import { Injectable, Provider } from '@angular/core';
-// import { Observable } from 'rxjs';
-
-// const apiURL: string = 'http://localhost:3030';
-
-// @Injectable()
-// class AppInterceptor implements HttpInterceptor {
-//   API = '/api';
-//   intercept(
-//     req: HttpRequest<any>,
-//     next: HttpHandler
-//   ): Observable<HttpEvent<any>> {
-//     if (req.url.startsWith(this.API)) {
-//       req = req.clone({
-//         url: req.url.replace(this.API, apiURL),
-//         withCredentials: true,
-//       });
-//     }
-//     console.log(req);
-    
-//     return next.handle(req);
-//   }
-// }
-
-// export const AppInterceptorProvider: Provider = {
-//   useClass: AppInterceptor,
-//   multi: true,
-//   provide: HTTP_INTERCEPTORS,
-// };
-
 import {
   HTTP_INTERCEPTORS,
   HttpEvent,
   HttpHandler,
-  HttpHeaders,
   HttpInterceptor,
   HttpRequest,
 } from '@angular/common/http';
@@ -55,22 +17,14 @@ class AppInterceptor implements HttpInterceptor {
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    let headers = new HttpHeaders(); 
     if (req.url.startsWith(this.API)) {
-      const accessToken = localStorage.getItem('[user]')
-        ? JSON.parse(localStorage.getItem('[user]')!).accessToken
-        : null;
-      if (accessToken) {
-        headers = headers.set('x-authorization', accessToken); 
-        req = req.clone({
-          headers,
-          url: req.url.replace(this.API, apiURL),
-          withCredentials: true,
-        });
-      }
+      req = req.clone({
+        url: req.url.replace(this.API, apiURL),
+        withCredentials: true,
+      });
     }
     console.log(req);
-
+    
     return next.handle(req);
   }
 }
@@ -80,4 +34,50 @@ export const AppInterceptorProvider: Provider = {
   multi: true,
   provide: HTTP_INTERCEPTORS,
 };
+
+// import {
+//   HTTP_INTERCEPTORS,
+//   HttpEvent,
+//   HttpHandler,
+//   HttpHeaders,
+//   HttpInterceptor,
+//   HttpRequest,
+// } from '@angular/common/http';
+// import { Injectable, Provider } from '@angular/core';
+// import { Observable } from 'rxjs';
+
+// const apiURL: string = 'http://localhost:3030';
+
+// @Injectable()
+// class AppInterceptor implements HttpInterceptor {
+//   API = '/api';
+//   intercept(
+//     req: HttpRequest<any>,
+//     next: HttpHandler
+//   ): Observable<HttpEvent<any>> {
+//     let headers = new HttpHeaders(); 
+//     if (req.url.startsWith(this.API)) {
+//       const accessToken = localStorage.getItem('[user]')
+//         ? JSON.parse(localStorage.getItem('[user]')!).accessToken
+//         : null;
+//       if (accessToken) {
+//         headers = headers.set('x-authorization', accessToken); 
+//         req = req.clone({
+//           headers,
+//           url: req.url.replace(this.API, apiURL),
+//           withCredentials: true,
+//         });
+//       }
+//     }
+//     console.log(req);
+
+//     return next.handle(req);
+//   }
+// }
+
+// export const AppInterceptorProvider: Provider = {
+//   useClass: AppInterceptor,
+//   multi: true,
+//   provide: HTTP_INTERCEPTORS,
+// };
 
