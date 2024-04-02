77ed66b473dd4cf9a75c593c21ec8c96919fb4ab
PetkoKrPetkov <136084693+PetkoKrPetkov@users.noreply.github.com>
2024-04-02 18:52:17 +0200

    changed css of loading-spinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// import { AuthModule } from './auth/auth.module';
 import { AppInterceptorProvider } from './app.interceptor';
 
 @NgModule({
